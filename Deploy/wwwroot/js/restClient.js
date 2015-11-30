7ab82eb368b26fde6d72bd1469ba89cb32cd02b9
Dementey Vadim <dementey@outlook.com>
2015-12-01 00:11:14 +0300

    Deploy Data

---
+++ Deploy/wwwroot/js/restClient.js
@@ -0,0 +1,48 @@
+﻿
+function restClient(url)
+{
+    this.url = url;
+}
+
+
+function processRequest (callBack, response)
+{
+    if (callBack)
+    {
+        if (response && response.responseText)
+        {
+            response = JSON.parse(response.responseText);
+        }
+        else
+        {
+            response = {};
+        }
+
+        callBack(response);
+    }
+}
+
+restClient.prototype.GET = function (callBack)
+{
+    this.callBack = callBack;
+    createHttpClient("GET", this.url, "application/json", function (x) { processRequest(callBack, x); }).send();
+}
+
+
+restClient.prototype.POST = function (dataObj, callBack)
+{
+    createHttpClient("POST", this.url, "application/json", function (x) { processRequest(callBack, x); }).send(JSON.stringify(dataObj));
+}
+
+
+restClient.prototype.PUT = function (dataObj, callBack)
+{
+    this.callBack = callBack;
+    createHttpClient("PUT", this.url, "application/json", function (x) { processRequest(callBack, x); }).send(JSON.stringify(dataObj));
+}
+
+restClient.prototype.DELETE = function (callBack)
+{
+    this.callBack = callBack;
+    createHttpClient("DELETE", this.url, "application/json", function (x) { processRequest(callBack, x); }).send();
+}
